182768c593aacfa667116b0a73b3899c93a2513a
ben041 <benmalunga041@gmail.com>
2025-09-07 12:47:11 -0700

    cleanup

--- nyasabox/core/forms.py
+++ nyasabox/core/forms.py
@@ -1,158 +1,137 @@
 from django import forms
-from .models import Album, Track, Comment
-
-from django import forms
-from .models import Album, Track, Comment, GENRE_CHOICES
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.validators import validate_email, FileExtensionValidator
+from django.core.exceptions import ValidationError
+from .models import Album, Track, Comment, Profile, DistributionRequest, DistributionPlatform, GENRE_CHOICES
 
 class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
         fields = ['title', 'artist', 'genre', 'release_date', 'cover_art', 'description']
         widgets = {
-            'release_date': forms.DateInput(attrs={'type': 'date', 'required': True}),
-            'description': forms.Textarea(attrs={'rows': 4}),
-            'artist': forms.TextInput(attrs={'placeholder': 'Enter artist name', 'required': True}),
-            'genre': forms.Select(attrs={'required': True}, choices=GENRE_CHOICES),
-            'title': forms.TextInput(attrs={'required': True}),
-            'cover_art': forms.FileInput(attrs={'required': True}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'artist': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter artist name', 'required': True}),
+            'genre': forms.Select(attrs={'class': 'form-control', 'required': True}, choices=GENRE_CHOICES),
+            'release_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': True}),
+            'cover_art': forms.FileInput(attrs={'class': 'form-control', 'required': True}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
         }
+
+    cover_art = forms.ImageField(
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif'])]
+    )
 
 class TrackForm(forms.ModelForm):
     class Meta:
         model = Track
         fields = ['title', 'album', 'artist', 'genre', 'audio_file', 'track_number']
         widgets = {
-            'track_number': forms.NumberInput(attrs={'min': 1}),
-            'artist': forms.TextInput(attrs={'placeholder': 'Enter artist name', 'required': True}),
-            'genre': forms.Select(attrs={'required': True}, choices=GENRE_CHOICES),
-            'title': forms.TextInput(attrs={'required': True}),
-            'audio_file': forms.FileInput(attrs={'required': True}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'album': forms.Select(attrs={'class': 'form-control'}),
+            'artist': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter artist name', 'required': True}),
+            'genre': forms.Select(attrs={'class': 'form-control', 'required': True}, choices=GENRE_CHOICES),
+            'audio_file': forms.FileInput(attrs={'class': 'form-control', 'required': True}),
+            'track_number': forms.NumberInput(attrs={'class': 'form-control', 'min': 1}),
         }
-    
+
+    audio_file = forms.FileField(
+        validators=[FileExtensionValidator(allowed_extensions=['mp3', 'wav'])]
+    )
+
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
-        # Only show albums uploaded by the current user
-        if 'initial' in kwargs and 'user' in kwargs['initial']:
-            user = kwargs['initial']['user']
+        if user:
             self.fields['album'].queryset = Album.objects.filter(uploader=user)
-        
-        # Make album field not required
         self.fields['album'].required = False
         self.fields['track_number'].required = False
-
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text']
         widgets = {
-            'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add your comment...'}),
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Add your comment...', 'required': True}),
         }
-        
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm
-from .models import Profile
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-    
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'required': True}))
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'required': True}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio', 'location', 'birth_date', 'profile_picture', 'website']
         widgets = {
-            'bio': forms.Textarea(attrs={'rows': 4}),
-            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'birth_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'profile_picture': forms.FileInput(attrs={'class': 'form-control'}),
+            'website': forms.URLInput(attrs={'class': 'form-control'}),
         }
-        
-        
-from django import forms
-from .models import DistributionRequest, DistributionPlatform, Track
 
-from django import forms
-from .models import DistributionRequest, DistributionPlatform, Track
+    profile_picture = forms.ImageField(
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'gif'])],
+        required=False
+    )
 
 class DistributionRequestForm(forms.ModelForm):
     platforms = forms.ModelMultipleChoiceField(
         queryset=DistributionPlatform.objects.filter(is_active=True),
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
         required=True
     )
-    
     tracks = forms.ModelMultipleChoiceField(
-        queryset=Track.objects.none(),  # Will be set in __init__
-        widget=forms.CheckboxSelectMultiple,
+        queryset=Track.objects.none(),
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
         required=True
     )
-    
+
     class Meta:
         model = DistributionRequest
-        fields = ['platforms', 'tracks']  # Only include many-to-many fields
-    
+        fields = ['platforms', 'tracks']
+
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
+        user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
-        
-        if self.user:
-            # Only show tracks that belong to the user
-            user_tracks = Track.objects.filter(uploader=self.user)
-            
-            # Exclude tracks that are already distributed (status = distributed)
+        if user:
             distributed_track_ids = DistributionRequest.objects.filter(
-                tracks__in=user_tracks,
+                tracks__in=Track.objects.filter(uploader=user),
                 status='distributed'
             ).values_list('tracks__id', flat=True).distinct()
-            
-            self.fields['tracks'].queryset = user_tracks.exclude(
-                id__in=distributed_track_ids
-            )
-    
+            self.fields['tracks'].queryset = Track.objects.filter(uploader=user).exclude(id__in=distributed_track_ids)
+
     def clean_tracks(self):
         tracks = self.cleaned_data.get('tracks')
         if not tracks:
             raise forms.ValidationError("Please select at least one track.")
         return tracks
-    
-    def save(self, commit=True):
-        # Don't try to save many-to-many relationships here
-        # They will be handled by save_m2m() after the instance is saved
-        instance = super().save(commit=False)
-        if commit:
-            instance.save()
-            self.save_m2m()
-        return instance
-    
-
-
-
-
-# authentication forms 
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Enter your email'
-    }))
-    
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs.update({'class': 'form-control'})
-    
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'required': True}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'required': True}),
+        }
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
@@ -162,13 +141,12 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
         label='Username or Email',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'required': True})
     )
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['password'].widget.attrs.update({'class': 'form-control'})
-    
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'required': True})
+    )
+
     def clean_username(self):
         username = self.cleaned_data.get('username')
         if '@' in username:
--- nyasabox/core/models.py
+++ nyasabox/core/models.py
@@ -2,113 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.urls import reverse
-
-from django.db import models
-from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db import models
-from django.contrib.auth.models import User
-from django.utils.text import slugify
-from django.urls import reverse
-from django.core.validators import MinValueValidator, MaxValueValidator
-from decimal import Decimal  # Add this import
+from decimal import Decimal
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.png')
-    website = models.URLField(blank=True)
-    
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
-# Signal to create profile when user is created
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-# Signal to save profile when user is saved
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-class BlogCategory(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-    
-    def __str__(self):
-        return self.name
-
-class BlogPost(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(BlogCategory, on_delete=models.SET_NULL, null=True)
-    content = models.TextField()
-    featured_image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    views = models.PositiveIntegerField(default=0)
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-    
-    def get_absolute_url(self):
-        return reverse('blog_detail', kwargs={'slug': self.slug})
-    
-    def __str__(self):
-        return self.title
-    
-from django.db import models
-from django.contrib.auth.models import User
-from django.utils.text import slugify
-from django.urls import reverse
-
-class Genre(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-    
-    def __str__(self):
-        return self.name
-
-class Artist(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    bio = models.TextField(blank=True)
-    image = models.ImageField(upload_to='artists/', blank=True, null=True)
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-    
-    def __str__(self):
-        return self.name
-
-from django.db import models
-from django.contrib.auth.models import User
-from django.utils.text import slugify
-from django.urls import reverse
-from django.core.validators import MinValueValidator, MaxValueValidator
-
-# Remove Artist model and update Genre to be choices
+# Genre choices
 GENRE_CHOICES = (
     ('afrobeat', 'Afrobeat'),
     ('amapiano', 'Amapiano'),
@@ -131,11 +30,79 @@ GENRE_CHOICES = (
     ('other', 'Other'),
 )
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.png')
+    website = models.URLField(blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user'], name='idx_profile_user'),
+        ]
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+@receiver(post_save, sender=User)
+def manage_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
+
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=100)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['slug'], name='idx_blogcategory_slug'),
+        ]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(BlogCategory, on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['slug'], name='idx_blogpost_slug'),
+            models.Index(fields=['created_at'], name='idx_blogpost_created_at'),
+        ]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog_detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
+
 class Album(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    artist = models.CharField(max_length=200)  # Changed from ForeignKey to CharField
-    genre = models.CharField(max_length=20, choices=GENRE_CHOICES)  # Changed from ForeignKey to CharField
+    slug = models.SlugField(unique=True, max_length=200)
+    artist = models.CharField(max_length=200)
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES)
     release_date = models.DateField()
     cover_art = models.ImageField(upload_to='album_covers/')
     description = models.TextField(blank=True)
@@ -143,24 +110,31 @@ class Album(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     downloads = models.PositiveIntegerField(default=0)
-    
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['slug'], name='idx_album_slug'),
+            models.Index(fields=['created_at'], name='idx_album_created_at'),
+            models.Index(fields=['uploader'], name='idx_album_uploader'),
+        ]
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
-    
+
     def get_absolute_url(self):
         return reverse('album_detail', kwargs={'slug': self.slug})
-    
+
     def __str__(self):
         return f"{self.title} by {self.artist}"
 
 class Track(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, max_length=200)
     album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='tracks', null=True, blank=True)
-    artist = models.CharField(max_length=200)  # Changed from ForeignKey to CharField
-    genre = models.CharField(max_length=20, choices=GENRE_CHOICES, blank=True)  # Changed from ForeignKey to CharField
+    artist = models.CharField(max_length=200)
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES, blank=True)
     audio_file = models.FileField(upload_to='tracks/')
     duration = models.DurationField(blank=True, null=True)
     track_number = models.PositiveIntegerField(null=True, blank=True)
@@ -169,17 +143,25 @@ class Track(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     downloads = models.PositiveIntegerField(default=0)
     likes = models.ManyToManyField(User, related_name='liked_tracks', blank=True)
-    
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['slug'], name='idx_track_slug'),
+            models.Index(fields=['created_at'], name='idx_track_created_at'),
+            models.Index(fields=['uploader'], name='idx_track_uploader'),
+            models.Index(fields=['downloads'], name='idx_track_downloads'),
+        ]
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         if not self.genre and self.album:
             self.genre = self.album.genre
         super().save(*args, **kwargs)
-    
+
     def get_absolute_url(self):
         return reverse('track_detail', kwargs={'slug': self.slug})
-    
+
     def get_formatted_duration(self):
         if self.duration:
             total_seconds = int(self.duration.total_seconds())
@@ -187,7 +169,7 @@ class Track(models.Model):
             seconds = total_seconds % 60
             return f"{minutes}:{seconds:02d}"
         return "0:00"
-    
+
     def __str__(self):
         return self.title
 
@@ -197,17 +179,27 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user'], name='idx_comment_user'),
+            models.Index(fields=['created_at'], name='idx_comment_created_at'),
+        ]
+
     def __str__(self):
         return f"Comment by {self.user.username} on {self.track.title if self.track else self.album.title}"
-    
-    
+
 class DistributionPlatform(models.Model):
     name = models.CharField(max_length=100)
     logo = models.ImageField(upload_to='platform_logos/', blank=True, null=True)
     description = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
-    
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name'], name='idx_platform_name'),
+        ]
+
     def __str__(self):
         return self.name
 
@@ -220,7 +212,7 @@ class DistributionRequest(models.Model):
         ('rejected', 'Rejected'),
         ('cancelled', 'Cancelled'),
     )
-    
+
     artist = models.ForeignKey(User, on_delete=models.CASCADE, related_name='distribution_requests')
     tracks = models.ManyToManyField(Track, related_name='distribution_requests')
     platforms = models.ManyToManyField(DistributionPlatform, related_name='distribution_requests')
@@ -230,21 +222,24 @@ class DistributionRequest(models.Model):
     payment_reference = models.CharField(max_length=100, blank=True)
     payment_date = models.DateTimeField(null=True, blank=True)
     distributed_date = models.DateTimeField(null=True, blank=True)
-    
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['artist'], name='idx_distribution_artist'),
+            models.Index(fields=['requested_at'], name='idx_distribution_requested_at'),
+            models.Index(fields=['status'], name='idx_distribution_status'),
+        ]
+
     def __str__(self):
         return f"Distribution Request #{self.id} - {self.artist.username}"
-    
+
     def calculate_total(self):
-        # 10,000 MWK for 6 songs, so ~1,667 MWK per song
-        base_price_per_track = Decimal('1666.67')
-        track_count = self.tracks.count()
-        return track_count * base_price_per_track
-    
+        return Decimal(str(self.tracks.count() * 1666.67))
+
     def get_track_count(self):
         return self.tracks.count()
-    
-    # def save(self, *args, **kwargs):
-    #     # Calculate total amount before saving
-    #     if not self.total_amount or self.tracks.exists():
-    #         self.total_amount = self.calculate_total()
-    #     super().save(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if not self.total_amount or self.tracks.exists():
+            self.total_amount = self.calculate_total()
+        super().save(*args, **kwargs)
