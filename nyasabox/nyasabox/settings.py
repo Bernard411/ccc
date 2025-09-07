677d2ddeda432a98ca23e1568914ef2ce3f9a681
ben041 <benmalunga041@gmail.com>
2025-09-07 13:03:13 -0700

    email config

--- nyasabox/nyasabox/settings.py
+++ nyasabox/nyasabox/settings.py
@@ -131,3 +131,14 @@ MEDIA_ROOT = BASE_DIR / "media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'premium160.web-hosting.com:'
+EMAIL_PORT=465
+EMAIL_USE_SSL=True
+EMAIL_USE_TLS=False
+EMAIL_HOST_USER = 'nyasabox@pezamw.com'
+EMAIL_HOST_PASSWORD = 'w{V}Jj!Go0r7'
+DEFAULT_FROM_EMAIL = 'NyasaBox <nyasabox@pezamw.com>'
