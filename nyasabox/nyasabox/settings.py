7e3e4c42ca6eb98bd61dce0200e4231ef407aa9b
Bernard411 <hamiebernard@gmail.com>
2025-09-09 06:50:37 -0500

    ks

--- nyasabox/nyasabox/settings.py
+++ nyasabox/nyasabox/settings.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'nyasabox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'pezaqbcv_nyasabox',        # Database name
-#         'USER': 'pezaqbcv_pezamw',        # Database user
-#         'PASSWORD': 'afriresearch2025',         # User password
-#         'HOST': 'localhost',                 # Use '127.0.0.1' or 'localhost'
-#         'PORT': '3306',                      # Default MySQL port
-#         'OPTIONS': {
-#             'sql_mode': 'STRICT_TRANS_TABLES',  # Recommended for consistency
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pezaqbcv_nyasabox',        # Database name
+        'USER': 'pezaqbcv_pezamw',        # Database user
+        'PASSWORD': 'afriresearch2025',         # User password
+        'HOST': 'localhost',                 # Use '127.0.0.1' or 'localhost'
+        'PORT': '3306',                      # Default MySQL port
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',  # Recommended for consistency
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
